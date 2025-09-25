7fc475530ccf0bdcdd3330919deb101e5d9e3a29
jonbehra <165320382+jonbehra@users.noreply.github.com>
2025-09-25 20:42:24 +0200

    1

--- main.py
+++ main.py
@@ -61,3 +61,41 @@ figg = px.bar(top_authors, x="Count", y="Author",orientation="h",
               color='Count', color_continuous_scale=px.colors.sequential.Plasma
               )
 st.plotly_chart(figg)
+
+# Interactivity: Filter Data by Genre
+st.subheader("Filter Data by Genre")
+genre_filter = st.selectbox("Select Genre", books_df['Genre'].unique())
+filtered_df = books_df[books_df['Genre'] == genre_filter]
+st.write(filtered_df)
+
+st.sidebar.header("Add a new book data")
+with st.sidebar.form("book_form"):
+    new_name= st.text_input("Book Name")
+    new_author = st.text_input("Author Name")
+    new_user_rating = st.slider("User Rating", 0.0,5.0,0.0,0.1)
+    new_reviews = st.number_input("Reviews", min_value=0, step=1)
+    new_price = st.number_input("Price", min_value=0, step=1)
+    new_year = st.number_input("Year", min_value=2009, max_value=2022,step=1)
+    new_genre = st.selectbox("Genre", books_df['Genre'].unique())
+    submit_button = st.form_submit_button(label="Add Book")
+
+if submit_button:
+    new_data = {
+        'Name': new_name,
+        'Author': new_author,
+        'User Rating': new_user_rating,
+        'Reviews': new_reviews,
+        'Price': new_price,
+        'Year': new_year,
+        'Genre': new_genre
+    }
+    books_df = pd.concat([pd.DataFrame(new_data,index=[0]),books_df],ignore_index=True)
+    books_df.to_csv('file.csv',index=False)
+    st.sidebar.success("New book added successfully")
+
+st.sidebar.header("Filter Option")
+selected_author = st.sidebar.selectbox("Select Author",["All"] + list(books_df['Author'].unique()))
+selected_year =  st.sidebar.selectbox("Select Year",["All"] + list(books_df['Year'].unique()))
+selected_genre = st.sidebar.selectbox("Select Genre",["All"] + list(books_df['Genre'].unique()))
+min_rating = st.sidebar.slider("Minimum User Rating", 0.0,5.0,0.0,0.1)
+max_price = st.sidebar.slider("Max Price",0,books_df['Price'].max(),books_df['Price'].max())
